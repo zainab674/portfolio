2d8ac309e1e255ddeba309596f07179ef34e7800
Zainab <zainabsarwar58@gmail.com>
2023-09-04 11:49:26 +0500

    relevant message

---
+++ index.js
@@ -0,0 +1,29 @@
+
+function myFunction1() {
+    var x = document.getElementById("educationshow");
+    var y = document.getElementById("languageshow");
+    var z = document.getElementById("experienceshow");
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+    
+}
+function myFunction2() {
+    var x = document.getElementById("educationshow");
+    var y = document.getElementById("languageshow");
+    var z = document.getElementById("experienceshow");
+        y.style.display = "block";
+        x.style.display = "none";
+        z.style.display = "none";
+        
+    
+}
+function myFunction3() {
+    var x = document.getElementById("educationshow");
+    var y = document.getElementById("languageshow");
+    var z = document.getElementById("experienceshow");
+        z.style.display = "block";
+        x.style.display = "none";
+        y.style.display = "none";
+   
+}
